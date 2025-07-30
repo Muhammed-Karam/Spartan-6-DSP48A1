4c1424140cfb613b4f23230ad462492da94b883c
Muhammed-Karam <mohamedkaram11011@gmail.com>
2025-07-30 18:24:28 +0300

    RTL design code

---
+++ RTL design/DFF.v
@@ -0,0 +1,25 @@
+module DFF(clk, Q, D, rst, CE);
+    parameter WIDTH = 1;
+    parameter RSTTYPE = "SYNC";
+    input clk, rst, CE;
+    input [WIDTH-1:0] D;
+    output reg [WIDTH-1:0] Q;
+    generate
+        if (RSTTYPE == "ASYNC") begin
+            always @(posedge clk or posedge rst) begin
+                if (rst) 
+                    Q <= {WIDTH{1'b0}};
+                else if (CE)
+                    Q <= D;
+            end
+        end
+        else begin // If -> RSTTYPE = "SYNC"
+            always @(posedge clk) begin
+                if (rst) 
+                    Q <= {WIDTH{1'b0}};
+                else if (CE)
+                    Q <= D;
+            end
+        end
+    endgenerate
+endmodule
